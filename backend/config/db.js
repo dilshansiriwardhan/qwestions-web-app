bb76bb5d0aefde2ce2f4f6ba920c7586a47cf767
Dila <dilshansiriwardhan@gmail.com>
2024-12-18 09:05:31 +0530

    Last

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
+console.log(process.env.MONGO_URI);
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
