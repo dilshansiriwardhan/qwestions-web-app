c7621f1eae114d6d9a9ea9b85bb905f79cc9ff87
Dila <dilshansiriwardhan@gmail.com>
2024-12-18 08:13:10 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ const app = express();
 app.use(cors({
   origin: process.env.BACKEND_URL,
   methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
