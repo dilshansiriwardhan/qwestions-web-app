d0d59af6d87aba749e37e41068a7976f89f707ec
Dila <dilshansiriwardhan@gmail.com>
2024-12-18 11:28:34 +0530

    Wow

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,6 @@ console.log(process.env.MONGO_URI);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('MongoDB connected successfully');
   } catch (err) {
